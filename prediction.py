7b4f284719bbc0b933a64c3e9677c63e9e5e353c
jasman <jasman0603@gmail.com>
2025-12-18 22:56:54 +0700

    update Mlfow test

--- prediction.py
+++ prediction.py
@@ -9,6 +9,8 @@ from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKF
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import seaborn as sns
+import mlflow
+import mlflow.sklearn 
 
 
 
@@ -33,19 +35,35 @@ preprocessor = ColumnTransformer(
     ]
 )
 
-
 pipeline = Pipeline(steps=[
-    ('scaler', preprocessor),
+    ('preprocessor', preprocessor),
     ('model', LinearRegression())
 ])
 
-pipeline.fit(X_train, y_train)
-y_pred = pipeline.predict(X_test)
 
-mae = mean_absolute_error(y_test, y_pred)
-rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-r2 = r2_score(y_test, y_pred)
+mlflow.set_experiment("Life Expectancy - Linear Regression")
 
-print(f"MAE  : {mae:.2f}")
-print(f"RMSE : {rmse:.2f}")
-print(f"R²   : {r2:.3f}")
+with mlflow.start_run(run_name="LinearRegression_v1"):
+
+    pipeline.fit(X_train, y_train)
+    y_pred = pipeline.predict(X_test)
+
+    mae = mean_absolute_error(y_test, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    r2 = r2_score(y_test, y_pred)
+
+    # ---- Log metrics
+    mlflow.log_metric("MAE", mae)
+    mlflow.log_metric("RMSE", rmse)
+    mlflow.log_metric("R2", r2)
+
+    # ---- Log model
+    mlflow.sklearn.log_model(
+        pipeline,
+        name="model",
+        registered_model_name="LifeExpectancyLinearModel"
+    )
+
+    print(f"MAE  : {mae:.2f}")
+    print(f"RMSE : {rmse:.2f}")
+    print(f"R²   : {r2:.3f}")
